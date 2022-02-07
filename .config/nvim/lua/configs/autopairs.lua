95fe8bf86fa4f9d93c17bcf4acdbef06b9cdd209
Max Pushkarov <maxpushka200@gmail.com>
2022-02-07 10:26:12 +0200

    feat(nvim): add new plugins, finally fix lualine - fix lualine crash on startup - add keymap to select all and copy content - change keymap go to next/prev diagnostic message - add new plugins   - ThePrimeagen/git-worktree.nvim   - ThePrimeagen/refactoring.nvim   - ray-x/lsp_signature.nvim   - rmagatti/goto-preview   - karb94/neoscroll.nvim   - luukvbaal/stabilize.nvim   - Pocco81/AutoSave.nvim   - Pocco81/TrueZen.nvim   - glepnir/dashboard-nvim

--- .config/nvim/lua/configs/autopairs.lua
+++ .config/nvim/lua/configs/autopairs.lua
@@ -2,14 +2,11 @@ local present1, autopairs = pcall(require, "nvim-autopairs")
 local present2, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
 
 if present1 and present2 then
-  local default = { 
+  local default = {
     fast_wrap = {},
     disable_filetype = { "TelescopePrompt" , "vim" },
     map_cr = true,
   }
-  if override_flag then
-      default = require("core.utils").tbl_override_req("nvim_autopairs", default)
-  end
   autopairs.setup(default)
 
   local cmp = require "cmp"
--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -2,18 +2,19 @@ local present, lualine = pcall(require, "lualine")
 if (not present) then return end
 
 local gps = require("nvim-gps")
+gps.setup()
 
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox_dark',
+    theme = 'nord',
     section_separators = {
-      left = '',  --'',
-      right = '', --''
+      left = '',
+      right = ''
     },
     component_separators = {
-      left = '',  --'',
-      right = '', --''
+      left = '',
+      right = ''
     },
     disabled_filetypes = {}
   },
@@ -26,7 +27,7 @@ lualine.setup {
         file_status = true, -- displays file status (readonly status, modified status)
         path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
       },
-      { gps.get_location, cond = gps.is_available },
+      { gps.get_location, cond = gps.is_available }
     },
     lualine_x = {
       { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
@@ -39,25 +40,14 @@ lualine.setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {
-      {
-        'filename',
-        file_status = true, -- displays file status (readonly status, modified status)
-        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-      },
-      { gps.get_location, cond = gps.is_available },
-    },
+    lualine_c = {{
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    }},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
-  }, 
+  },
   extensions = {'fugitive'},
-  -- tabline = {
-  --   lualine_a = {},
-  --   lualine_b = {},
-  --   lualine_c = {},
-  --   lualine_x = {},
-  --   lualine_y = {},
-  --   lualine_z = {},
-  -- },
 }
