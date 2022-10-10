aeac1176ce012f1a60a4697698f2b5ac0ab82060
Max Pushkarov <maxpushka200@gmail.com>
2022-10-10 17:00:35 +0300

    feat(nvim): define keybindings for gitsigns
    add file ignore patters for telescope; format core config

--- .config/nvim/lua/core.lua
+++ .config/nvim/lua/core.lua
@@ -1,22 +1,21 @@
 -- Fundamentals --{{{
 -- ---------------------------------------------------------------------
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.cmd('autocmd!')
+vim.cmd("autocmd!")
 vim.o.swapfile = false
 vim.opt.compatible = false
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = 'a'
-vim.api.nvim_command('syntax enable')
-vim.o.fileencodings = 'utf-8,sjis,euc-jp,latin,cp1251,koi8-r,cp866'
-vim.o.encoding = 'utf-8'
-vim.o.guifont = '*'
-vim.o.guifontwide = '*'
+vim.o.mouse = "a"
+vim.api.nvim_command("syntax enable")
+vim.o.fileencodings = "utf-8,sjis,euc-jp,latin,cp1251,koi8-r,cp866"
+vim.o.encoding = "utf-8"
+vim.o.guifont = "*"
+vim.o.guifontwide = "*"
 vim.o.title = true
-vim.o.autoindent = true
-vim.o.background = 'dark'
+vim.o.background = "dark"
 vim.o.backup = false
 vim.o.hlsearch = true
 vim.o.showcmd = true
@@ -25,11 +24,11 @@ vim.o.laststatus = 3
 vim.o.scrolloff = 10
 vim.o.expandtab = true
 --vim.o.loaded_matchparen = 1
-vim.o.shell = 'bash'
-vim.o.backupskip = '/tmp/*,/private/tmp/*'
+vim.o.shell = "bash"
+vim.o.backupskip = "/tmp/*,/private/tmp/*"
 
 -- incremental substitution
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- Suppress appending <PasteStart> and <PasteEnd> when pasting
 --vim.o.t_BE=''
@@ -44,28 +43,29 @@ vim.o.lazyredraw = true
 vim.o.ignorecase = true -- Ignore case when searching
 vim.o.smarttab = true -- Be smart when using tabs ;)
 -- indents
-vim.api.nvim_command('filetype plugin indent on')
+vim.api.nvim_command("filetype plugin indent on")
+vim.o.autoindent = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.ai = true -- Auto indent
 vim.o.si = true -- Smart indent
 vim.o.wrap = false -- No Wrap lines
-vim.o.backspace = 'start,eol,indent'
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.backspace = "start,eol,indent"
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldlevel = 20
 -- Finding files - Search down into subfolders
-vim.opt.path:append('**')
-vim.opt.wildignore:append('*/node_modules/*')
+vim.opt.path:append("**")
+vim.opt.wildignore:append("*/node_modules/*")
 
 -- Turn off paste mode when leaving insert
-vim.cmd('autocmd InsertLeave * set nopaste')
+vim.cmd("autocmd InsertLeave * set nopaste")
 
 -- Add asterisks in block comments
-vim.opt.formatoptions:append('r')
+vim.opt.formatoptions:append("r")
 
 -- if set, when we switch between buffers, it will not split more than once. It will switch to the existing buffer instead
-vim.opt.switchbuf = 'useopen'
+vim.opt.switchbuf = "useopen"
 
 vim.o.exrc = true
 vim.o.clipboard = "unnamedplus"
@@ -123,8 +123,13 @@ au BufNewFile,BufRead *.flow set filetype=javascript
 set suffixesadd=.js,.es,.jsx,.json,.css,.less,.sass,.styl,.php,.py,.md
 
 autocmd FileType coffee setlocal shiftwidth=2 tabstop=2
-autocmd FileType ruby setlocal shiftwidth=2 tabstop=2
-autocmd FileType yaml setlocal shiftwidth=2 tabstop=2
+autocmd FileType ruby   setlocal shiftwidth=2 tabstop=2
+autocmd FileType yaml   setlocal shiftwidth=2 tabstop=2
+
+autocmd FileType javascript      setlocal shiftwidth=4 tabstop=4
+autocmd FileType typescript      setlocal shiftwidth=4 tabstop=4
+autocmd FileType javascriptreact setlocal shiftwidth=4 tabstop=4
+autocmd FileType typescriptreact setlocal shiftwidth=4 tabstop=4
 ]])
 
 --}}}
@@ -133,11 +138,11 @@ autocmd FileType yaml setlocal shiftwidth=2 tabstop=2
 -- ---------------------------------------------------------------------
 
 if vim.fn.has("unix") then
-  if vim.fn.system("uname -s") == "Darwin\n" then
-    require('macos')
-  elseif string.find(vim.fn.system("uname -r"), "microsoft") then
-    require('wsl')
-  end
+	if vim.fn.system("uname -s") == "Darwin\n" then
+		require("macos")
+	elseif string.find(vim.fn.system("uname -r"), "microsoft") then
+		require("wsl")
+	end
 end
 
 --}}}
@@ -147,16 +152,16 @@ end
 
 -- true color
 if vim.fn.exists("&termguicolors") and vim.fn.exists("&winblend") then
-  vim.cmd('syntax enable')
-  vim.o.termguicolors = true
-  vim.o.winblend = 0
-  vim.o.wildoptions = "pum"
-  vim.o.pumblend = 5
-  vim.o.background = "dark"
-  -- Use NeoSolarized
-  --vim.g.neosolarized_termtrans = 1
-  --vim.cmd('runtime ./colors/NeoSolarized.vim')
-  --vim.cmd("colorscheme NeoSolarized")
+	vim.cmd("syntax enable")
+	vim.o.termguicolors = true
+	vim.o.winblend = 0
+	vim.o.wildoptions = "pum"
+	vim.o.pumblend = 5
+	vim.o.background = "dark"
+	-- Use NeoSolarized
+	--vim.g.neosolarized_termtrans = 1
+	--vim.cmd('runtime ./colors/NeoSolarized.vim')
+	--vim.cmd("colorscheme NeoSolarized")
 end
 
 --}}}
