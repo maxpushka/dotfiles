7358ea24689e63d76f5ada7f6ab322575dcd19a0
Max Pushkarov <maxpushka200@gmail.com>
2024-03-07 20:37:27 +0200

    feat(nvim): add Go config for LunarVim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -82,6 +82,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "tsx",
   "css",
   "go",
+  "gomod",
   "rust",
   "java",
   "yaml",
@@ -189,14 +190,16 @@ lvim.plugins = {
 --   end,
 -- })
 
+
+
+-------------
+-- C++ CONFIG
+-------------
+
 vim.wo.relativenumber = true
 lvim.format_on_save = false
--- lvim.lsp.diagnostics.virtual_text = true
 vim.diagnostic.config({ virtual_text = true })
 lvim.builtin.treesitter.highlight.enable = true
-
--- auto install treesitter parsers
-lvim.builtin.treesitter.ensure_installed = { "cpp", "c" }
 
 -- Additional Plugins
 table.insert(lvim.plugins, {
@@ -215,9 +218,9 @@ local clangd_flags = {
   "--suggest-missing-includes",
   "--cross-file-rename",
   "--completion-style=detailed",
-  "--pch-storage=memory", -- could also be disk
+  "--pch-storage=memory",     -- could also be disk
   "--folding-ranges",
-  "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+  "--enable-config",          -- clangd 11+ supports reading from .clangd configuration file
   "--offset-encoding=utf-16", --temporary fix for null-ls
   -- "--limit-references=1000",
   -- "--limit-resutls=1000",
@@ -226,8 +229,6 @@ local clangd_flags = {
   -- "--header-insertion=never",
   -- "--query-driver=<list-of-white-listed-complers>"
 }
-
-local provider = "clangd"
 
 local custom_on_attach = function(client, bufnr)
   require("lvim.lsp").common_on_attach(client, bufnr)
@@ -243,8 +244,8 @@ local custom_on_attach = function(client, bufnr)
   require("clangd_extensions.inlay_hints").set_inlay_hints()
 end
 
-local status_ok, project_config = pcall(require, "rhel.clangd_wrl")
-if status_ok then
+local clangd_status_ok, project_config = pcall(require, "rhel.clangd_wrl")
+if clangd_status_ok then
   clangd_flags = vim.tbl_deep_extend("keep", project_config, clangd_flags)
 end
 
@@ -262,7 +263,7 @@ local custom_on_init = function(client, bufnr)
 end
 
 local opts = {
-  cmd = { provider, unpack(clangd_flags) },
+  cmd = { "clangd", unpack(clangd_flags) },
   on_attach = custom_on_attach,
   on_init = custom_on_init,
 }
@@ -305,3 +306,103 @@ lvim.builtin.dap.on_config_done = function(dap)
 
   dap.configurations.c = dap.configurations.cpp
 end
+
+
+
+-------------
+-- GO CONFIG
+-------------
+
+------------------------
+-- Plugins
+------------------------
+table.insert(lvim.plugins, {
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+})
+
+------------------------
+-- Formatting
+------------------------
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "goimports", filetypes = { "go" } },
+  { command = "gofumpt",   filetypes = { "go" } },
+}
+
+lvim.format_on_save = {
+  pattern = { "*.go" },
+}
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+------------------------
+-- LSP
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+    local map = function(mode, lhs, rhs, desc)
+      if desc then
+        desc = desc
+      end
+
+      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+    end
+    map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+    map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
+    map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
+    map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+    map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+    map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
+    map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+    map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
+    map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+local gopher_status_ok, gopher = pcall(require, "gopher")
+if not gopher_status_ok then
+  return
+end
+
+gopher.setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
